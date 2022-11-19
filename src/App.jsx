a6951c757f191123ead11b266d23b1a4e6516c34
Jhon Briones <joanbric@gmail.com>
2022-11-19 15:04:03 +0000

    Contador de clics finalizado

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import "./App.css";
+import freeCodeCampLogo from "./imgs/freeCodeCamp-logo.svg";
+import Boton from "./components/Boton";
+import Contador from "./components/Contador";
+import { useState } from "react";
+
+function App() {
+    const [numClics, setNumClics] = useState(0);
+
+    const manejarClic = () => {
+        setNumClics(numClics + 1);
+    };
+
+    const reiniciarContador = () => {
+        setNumClics(0);
+    };
+
+    return (
+        <div className="App">
+            <div className="freecodecamp-logo-contenedor">
+                <img
+                    src={freeCodeCampLogo}
+                    alt="Logo de freeCodeCamp"
+                    className="freecodecamp-logo"
+                />
+            </div>
+            <div className="contenedor-principal">
+                <Contador numClics={numClics} />
+
+                <Boton
+                    texto="Clic"
+                    esBotonDeClic={true}
+                    manejarClic={manejarClic}
+                />
+
+                <Boton
+                    texto="Reiniciar"
+                    esBotonDeClic={false}
+                    manejarClic={reiniciarContador}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default App;
